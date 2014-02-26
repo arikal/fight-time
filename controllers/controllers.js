4a9b677d38b713d8c6c7fec9fbaf7d23af525b81
iammartincollins <hi@iammartincollins.com>
2014-02-26 12:37:41 +0000

    Latest

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,13 +1,15 @@
 angular.module('FightTimeApp.controllers', []).
 controller('fightersController', function($scope) {
+    //var pOne = new Ninja();
     $scope.playerOne = [
       {
 	  	type: "Samurai",
-		health: 40,
+		health: 55,
 		attack: 50,
 		defence: 60,
 		speed: 10,
 		evade: 0.3,
       }
     ];
+
 });
--- models.js
+++ models.js
@@ -1,4 +1,12 @@
-function Warrior (attrs) {
+function Ninja (attrs) {
 	_.extend(this, attrs);
 }
+
+_.extend(Ninja.prototype, {
+	getHealth: function(){
+
+		h = Math.floor(Math.random() * 60) + 40;
+		return h;
+	}
+})
 
